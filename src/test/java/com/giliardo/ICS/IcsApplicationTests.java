e80f14ddc3216db44d5d6054c827cea1202b2673
gili-julio <giliardoplay@gmail.com>
2024-09-28 18:07:50 -0300

    Commit inicial

---
+++ src/test/java/com/giliardo/ICS/IcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giliardo.ICS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
