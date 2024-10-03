301718e5efcde90c467edbc53df65e74ccfe22ff
gili-julio <giliardoplay@gmail.com>
2024-10-03 16:08:38 -0300

    user-info page added

--- src/main/java/com/giliardo/ICS/controller/UsuarioController.java
+++ src/main/java/com/giliardo/ICS/controller/UsuarioController.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.giliardo.ICS.entity.Usuario;
 import com.giliardo.ICS.repository.UsuarioRepository;
+import com.giliardo.ICS.security.CustomUserDetailsService;
 
 @Controller
 @RequestMapping("/usuarios")
@@ -25,6 +29,26 @@ public class UsuarioController {
     @GetMapping
     public ResponseEntity<String> getUser() {
         return ResponseEntity.ok("sucesso!");
+    }
+
+    @GetMapping("/info")
+    public String usuarioInfo(Model model) {
+        // Obtém o usuário autenticado
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            // Se o usuário está autenticado
+            Object principal = authentication.getPrincipal();
+
+            if (principal instanceof UserDetails) {
+                String username = ((UserDetails) principal).getUsername();
+                System.out.println("Username: " + username);
+                Usuario usuario = usuarioRepository.findByEmail(username)
+                        .orElseThrow(() -> new RuntimeException("User not found"));
+                model.addAttribute("user", usuario);
+            }
+        }
+        return "usuarios/info";
     }
 
     @GetMapping("/listar")
